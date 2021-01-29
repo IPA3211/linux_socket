cadad4d29f49545b125b4ca7666fa9b04829b91b
ipa3211 <kygsm2003@naver.com>
2021-01-29 22:04:13 +0900

    add UDP connect client test

---
+++ src/mySocketTestUDPCClient.cpp
@@ -0,0 +1,34 @@
+#include "mySocket.h"
+
+int main(void) {
+	std::string ip;
+	short port;
+	std::string buffer;
+
+	std::cout << "Insert server IP : ";
+	std::cin >> ip;
+	std::cout << "insert port : ";
+	std::cin >> port;
+
+	MySocket servSock(UDP);
+
+	servSock.connect(ip, port);
+
+	while (1) {
+		std::cout << "send message : ";
+		std::cin >> buffer;
+
+		if (buffer.size() == 1) {
+			if (buffer[0] == 'q' || buffer[0] == 'Q') {
+				break;
+			}
+		}
+
+		servSock.send(buffer);
+
+		std::cout << "echo : " << servSock.recv();
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
